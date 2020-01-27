22d7d7d3c5bff3f24a10466658fb67b84dd8429d
Roviluca <Roviluca@users.noreply.github.com>
2020-01-27 22:26:29 +0100

    Implemented GetHost

--- host.go
+++ host.go
@@ -1,1 +1,78 @@
 package thruk
+
+import (
+	"encoding/json"
+)
+
+type Host struct {
+	FILE                       string   `json:":FILE"`
+	ID                         string   `json:":ID,omitempty"`
+	PEERKEY                    string   `json:":PEER_KEY,omitempty"`
+	READONLY                   int      `json:":READONLY,omitempty"`
+	TYPE                       string   `json:":TYPE"`
+	WORKER                     string   `json:"_WORKER,omitempty"`
+	ActiveChecksEnabled        string   `json:"active_checks_enabled,omitempty"`
+	Address                    string   `json:"address,omitempty"`
+	CheckCommand               string   `json:"check_command,omitempty"`
+	CheckInterval              string   `json:"check_interval,omitempty"`
+	CheckPeriod                string   `json:"check_period,omitempty"`
+	EventHandlerEnabled        string   `json:"event_handler_enabled,omitempty"`
+	FlapDetectionEnabled       string   `json:"flap_detection_enabled,omitempty"`
+	MaxCheckAttempts           string   `json:"max_check_attempts,omitempty"`
+	Name                       string   `json:"name"`
+	NotificationInterval       string   `json:"notification_interval,omitempty"`
+	NotificationOptions        []string `json:"notification_options,omitempty"`
+	NotificationPeriod         string   `json:"notification_period,omitempty"`
+	NotificationsEnabled       string   `json:"notifications_enabled,omitempty"`
+	ProcessPerfData            string   `json:"process_perf_data,omitempty"`
+	Register                   string   `json:"register,omitempty"`
+	RetainNonstatusInformation string   `json:"retain_nonstatus_information,omitempty"`
+	RetainStatusInformation    string   `json:"retain_status_information,omitempty"`
+	RetryInterval              string   `json:"retry_interval,omitempty"`
+	ActionURL                  string   `json:"action_url,omitempty"`
+	FailurePredictionEnabled   string   `json:"failure_prediction_enabled,omitempty"`
+	Alias                      string   `json:"alias,omitempty"`
+	Use                        []string `json:"use,omitempty"`
+	TwoDCoords                 string   `json:"2d_coords,omitempty"`
+	ThreeDCoords               string   `json:"3d_coords,omitempty"`
+	CheckFreshness             string   `json:"check_freshness,omitempty"`
+	ContactGroups              []string `json:"contact_groups,omitempty"`
+	Contacts                   []string `json:"contacts,omitempty"`
+	DisplayName                string   `json:"display_name,omitempty"`
+	FirstNotificationDelay     string   `json:"first_notification_delay,omitempty"`
+	FlapDetectionOptions       []string `json:"flap_detection_options,omitempty"`
+	FreshnessThreshold         string   `json:"freshness_threshold,omitempty"`
+	HighFlapThreshold          string   `json:"high_flap_threshold,omitempty"`
+	HostName                   string   `json:"host_name,omitempty"`
+	Hostgroups                 []string `json:"hostgroups,omitempty"`
+	IconImage                  string   `json:"icon_image,omitempty"`
+	IconImageAlt               string   `json:"icon_image_alt,omitempty"`
+	InitialState               string   `json:"initial_state,omitempty"`
+	LowFlapThreshold           string   `json:"low_flap_threshold,omitempty"`
+	Notes                      string   `json:"notes,omitempty"`
+	NotesURL                   string   `json:"notes_url,omitempty"`
+	ObsessOverHost             string   `json:"obsess_over_host,omitempty"`
+	Parents                    []string `json:"parents,omitempty"`
+	PassiveChecksEnabled       string   `json:"passive_checks_enabled,omitempty"`
+	StalkingOptions            []string `json:"stalking_options,omitempty"`
+	StatusmapImage             string   `json:"statusmap_image,omitempty"`
+	VrmlImage                  string   `json:"vrml_image,omitempty"`
+}
+
+func (t Thruk) GetHost(id string) (Host, error) {
+	var hosts []Host
+	if id == "" {
+		return Host{}, ErrorInvalidInput
+	}
+	resp, err := t.GetURL("/" + t.SiteName + "/thruk/r/config/objects?:TYPE=host&:ID=" + id)
+	failOnError(err)
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&hosts)
+	failOnError(err)
+	if len(hosts) == 0 {
+		return Host{}, ErrorObjectNotFound
+	}
+
+	return hosts[0], nil
+}
--- host_test.go
+++ host_test.go
@@ -1,1 +1,38 @@
 package thruk
+
+import (
+	"gotest.tools/assert"
+	"testing"
+)
+
+func Test_thruk_client_GetConfigObject_Host(t *testing.T) {
+	t.Run("Get host of empty id returns error", func(t *testing.T) {
+		thruk := startThrukServerAndGetClient(t)
+
+		object, err := thruk.GetHost("")
+		assert.Error(t, err, "[ERROR] invalid input")
+		assert.DeepEqual(t, object, Host{})
+	})
+	t.Run("Get host from id returns host", func(t *testing.T) {
+		thruk := startThrukServerAndGetClient(t)
+
+		object, err := thruk.GetHost("8e4f0")
+		assert.NilError(t, err)
+		assert.DeepEqual(t, object, Host{
+			FILE:            "/omd/sites/demo/etc/naemon/conf.d/histou.cfg:5",
+			ID:              "8e4f0",
+			PEERKEY:         "48f1c",
+			READONLY:        0,
+			TYPE:            "host",
+			ActionURL:       "/demo/grafana/dashboard/script/histou.js?host=$HOSTNAME$&theme=light&annotations=true",
+			Name:            "host-perf",
+			ProcessPerfData: "1",
+			Register:        "0",
+		})
+	})
+	//	t.Run("Create host returns error when FILE and TYPE are empty", func(t *testing.T) {})
+	//	t.Run("Create host returns error if thruk returns error", func(t *testing.T) {})
+	//	t.Run("Create host returns nil error and ID on success", func(t *testing.T) {})
+	//	t.Run("Delete host must return nil error if object exists", func(t *testing.T) {})
+	//	t.Run("Delete host must remove an object that exists", func(t *testing.T) {})
+}
